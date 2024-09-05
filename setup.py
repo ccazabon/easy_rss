d3b2fd6ac17aba497262ff4130e379e91c9b1c6a
michaelmondoro <michaelmondoro@gmail.com>
2024-09-05 14:18:54 -0400

    once more

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="easy_rss",
-    version="1.2.3",
+    version="1.2.4",
     author="Michael Mondoro",
     author_email="michaelmondoro@gmail.com",
     description="Simple Python package for interacting with RSS feeds",
